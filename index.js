2a4e636577bdaaa0e2649f97a0be0128afb4ae18
Sebastião de Paula <ysebassdepaula308@gmail.com>
2025-05-07 17:49:54 +0000

    Aula 07/05/2025

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ app.use(express.static('public'));
 
 app.get('/', (request, response) => {
     //response.send('Testando!!!');
-    response.render('home');
+    response.render('home', {nome : "Sebastião"});
 });
+
+app.get('/tabuada', (request, response) => {
+resultado = [];
+for (let cont = 1;cont <=10;cont++){
+resultado.push(cont * 5);
+response.render('tabuada', {valores : resultado});
+}
+});
+
 
 app.listen(3000, () => {
     console.log('Servidor na porta 3000');
